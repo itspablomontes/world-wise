df54309629f72edecc8d0a10edb17d286b3f8f70
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-02-19 16:28:18 -0300

    feat: add loading state to homepage

---
+++ src/components/Loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+	return (
+		<div className="flex justify-center items-center w-full p-30 md:p-60">
+			<span className="loading scale-200" />
+		</div>
+	);
+};
+
+export default Loading;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from "react";
 import CountryItem from "../components/CountryItem";
 import type { CountryType } from "../types/Types";
 import api from "../services/api";
+import Loading from "../components/Loading";
 
 const Home = () => {
 	const [countries, setCountries] = useState<CountryType[]>([]);
-
+	const [isLoading, setIsLoading] = useState(true);
 	useEffect(() => {
 		const getAllCountries = async () => {
 			try {
@@ -13,22 +14,30 @@ const Home = () => {
 				setCountries(response.data);
 			} catch (error) {
 				console.log(error);
+			} finally {
+				setIsLoading(false);
 			}
 		};
 		getAllCountries();
 	}, []);
 
 	return (
-		<div className="flex flex-col gap-3 font-bold">
-			{countries.map((country) => (
-				<CountryItem
-					key={country.cca2}
-					name={country.name.common}
-					flag={country.flags.svg}
-					countryCode={country.cca2}
-				/>
-			))}
-		</div>
+		<>
+			<h1 className="font-bold text-center text-2xl md:text-4xl p-6">
+				🌎 World Wise
+			</h1>
+			{isLoading && <Loading />}
+			<div className="grid grid-cols-2 md:grid-cols-4 gap-3 font-bold cent">
+				{countries.map((country) => (
+					<CountryItem
+						key={country.cca2}
+						name={country.name.common}
+						flag={country.flags.svg}
+						countryCode={country.cca2}
+					/>
+				))}
+			</div>
+		</>
 	);
 };
 
