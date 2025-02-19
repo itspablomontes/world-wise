5ca3b6278867435e8c25204f158cd696ccf8dc95
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-02-19 19:09:38 -0300

    refac: extract CountryList component

---
+++ src/components/CountryList.tsx
@@ -0,0 +1,22 @@
+import type { CountryType } from "../types/Types";
+import CountryItem from "./CountryItem";
+type Props = {
+	filteredCountries: CountryType[];
+};
+
+const CountryList = ({ filteredCountries }: Props) => {
+	return (
+		<div className="grid grid-cols-2 md:grid-cols-4 gap-3 font-bold cent">
+			{filteredCountries.map((country) => (
+				<CountryItem
+					key={country.cca2}
+					name={country.name.common}
+					flag={country.flags.svg}
+					countryCode={country.cca2}
+				/>
+			))}
+		</div>
+	);
+};
+
+export default CountryList;
