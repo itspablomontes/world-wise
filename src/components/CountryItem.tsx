bf6a22834663e80ac6496a968d428b3e30c3d207
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-02-22 16:34:39 -0300

    style: add animation to country items

--- src/components/CountryItem.tsx
+++ src/components/CountryItem.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import { motion } from "motion/react";
 type Props = {
 	name: string;
 	flag: string;
@@ -8,16 +8,21 @@ type Props = {
 
 const CountryItem = ({ name, flag, countryCode }: Props) => {
 	return (
-		<Link
-			className="flex justify-center flex-col text-center h-50 min-w-full flex-1 border-2 border-amber-600 p-3 rounded-xl hover:bg-zinc-600"
-			to={`/${countryCode}`}
-		>
-			<img
-				src={flag}
-				alt={`${name} flag`}
-				className=" self-center rounded-lg h-full max-h-15 md:max-h-25"
-			/>
-			<div className="text-">{name}</div>
+		<Link to={`/${countryCode}`}>
+			<motion.div
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				whileHover={{ scale: 1.1 }}
+				whileTap={{ scale: 0.95 }}
+				className="flex justify-center flex-col text-center h-50 min-w-full flex-1 bg-zinc-800 drop-shadow-xl p-3 rounded-xl hover:bg-zinc-600"
+			>
+				<img
+					src={flag}
+					alt={`${name} flag`}
+					className=" self-center rounded-lg h-full max-h-15 md:max-h-25"
+				/>
+				<div className="text-">{name}</div>
+			</motion.div>
 		</Link>
 	);
 };
