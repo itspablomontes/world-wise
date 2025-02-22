623eddfd8a4df660bc3559e4eb96268bec972829
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-02-22 16:36:02 -0300

    style: tweak search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,5 @@
 import type { CountryType } from "../types/Types";
-
+import { motion } from "motion/react";
 type Props = {
 	value: string;
 	onChange: (value: string) => void;
@@ -30,12 +30,32 @@ const SearchBar = ({
 		}
 	};
 	return (
-		<input
-			className="outline-none rounded-lg p-1 md:w-1/5 w-4/5 "
-			type="search"
-			value={value}
-			onChange={handleChange}
-		/>
+		<motion.label
+			className="input  flex items-center gap-2  bg-zinc-800 rounded-xl md:p-2 md:text-lg"
+			initial={{ y: 100 }}
+			animate={{ y: 0 }}
+		>
+			<input
+				className="grow outline-none"
+				placeholder="Search"
+				type="search"
+				value={value}
+				onChange={handleChange}
+			/>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 16 16"
+				fill="currentColor"
+				className="h-4 w-4 opacity-70"
+			>
+				<title>Search</title>
+				<path
+					fillRule="evenodd"
+					d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+					clipRule="evenodd"
+				/>
+			</svg>
+		</motion.label>
 	);
 };
 
