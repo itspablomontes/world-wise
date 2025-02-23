20d04db47b861061c0ed1da38528820f942aba8d
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-02-23 17:39:41 -0300

    feat: create Location component

---
+++ src/components/Location.tsx
@@ -0,0 +1,26 @@
+import { Earth } from "lucide-react";
+
+type Props = {
+	region: string | undefined;
+	subRegion: string | undefined;
+	capital: string | undefined;
+};
+
+const Location = ({ region, subRegion, capital }: Props) => {
+	return (
+		<div className="flex flex-col items-center text-2xl">
+			<Earth size={50} />
+			<p>REGION</p>
+			<p className="font-extralight">{region}</p>
+			<div className="border-b-2 border-t-2 border-white text-center my-2">
+				<p>SUB-REGION</p>
+				<p className="font-extralight">{subRegion}</p>
+			</div>
+
+			<p>CAPITAL</p>
+			<p className="font-extralight">{capital}</p>
+		</div>
+	);
+};
+
+export default Location;
